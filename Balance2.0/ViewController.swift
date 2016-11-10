a1902fb6c7375fb6d9d129705bd44de4ddc63d02
Zell Nguyen <hoazell41195@gmail.com>
2016-11-10 01:04:35 -0800

    Initial Commit

---
+++ Balance2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Balance2.0
+//
+//  Created by Hoa Nguyen on 2016-11-10.
+//  Copyright © 2016 Hoa Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
